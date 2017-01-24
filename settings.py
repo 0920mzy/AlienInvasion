c754d3c09edbd4a87bd52c28e2e5eb1f1ad0dc4b
Ma Zhaoyu <jerryma@Jerrys-MacBook-Pro.local>
2017-01-24 22:18:41 +0800

    modified speed factor

--- settings.py
+++ settings.py
@@ -4,15 +4,15 @@ class Settings():
 		self.screen_height = 650
 		self.bg_color = (230,230,230)
 
-		self.ship_speed_factor = 10
+		self.ship_speed_factor = 5
 
-		self.bullet_speed_factor = 15
+		self.bullet_speed_factor = 5
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = 60, 60, 60
 		self.bullets_allowed = 10
 
-		self.alien_speed_factor = 100
+		self.alien_speed_factor = 3
 		self.fleet_drop_speed = 10
 		self.fleet_direction = 1
 
