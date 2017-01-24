8cfe69db282faa18a88b62d29b767a86e6db0854
Ma Zhaoyu <jerryma@Jerrys-MacBook-Pro.local>
2017-01-24 23:01:41 +0800

    Added reset function

--- game_function.py
+++ game_function.py
@@ -5,7 +5,7 @@ from bullet import Bullet
 from alien import Alien
 from time import sleep
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
@@ -15,11 +15,21 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
 			check_keyup_events(event, ship)
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			mouse_x, mouse_y = pygame.mouse.get_pos()
-			check_play_button(stats, play_button, mouse_x, mouse_y)
+			check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(stats, play_button, mouse_x,mouse_y):
-	if play_button.rect.collidepoint(mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x,mouse_y):
+	button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+	if button_clicked and not stats.game_active:
+		ai_settings.initialize_dynamic_settings()
+		pygame.mouse.set_visible(False)
+		stats.reset_stats()
 		stats.game_active = True
+
+		aliens.empty()
+		bullets.empty()
+
+		create_fleet(ai_settings, screen, ship, aliens)
+		ship.center_ship()
 
 def check_keyup_events(event, ship):
 	if event.key == pygame.K_RIGHT:
@@ -67,6 +77,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
 	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)	
 	if len(aliens) ==0:
 		bullets.empty()
+		ai_settings.increase_speed()
 		create_fleet(ai_settings,screen,ship,aliens)
 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
@@ -129,6 +140,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 		sleep(0.5)
 	else:
 		stats.game_active = False
+		pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
 	screen_rect = screen.get_rect()
